3f9c8448ff8a8d0f87015aca08c7aa57c983d4d6
eezn <55613344+eezn@users.noreply.github.com>
2020-06-01 13:33:57 +0900

    2.10

--- so.py
+++ so.py
@@ -10,11 +10,24 @@ URL = f"https://stackoverflow.com/jobs?q=pytho&sort=i"
 
 def get_last_page():
     result = requests.get(URL)
-    soup = BeautifulSoup(result.text, "html.parser")
+    soup = BeautifulSoup(result.text, "html.parser")  # soup -> Local Variable
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
-    print(pages)
+    last_page = pages[-2].get_text(strip=True)
+    return int(last_page)
+
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&pg={page+1}")
+        # soup -> Local Variable
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "-job"})
+        for result in results:
+            print(result["data-jobid"])
 
 
 def get_jobs():
     last_page = get_last_page()
-    return []
+    jobs = extract_jobs(last_page)
+    return jobs
